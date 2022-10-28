dcf5eafe713f939bd30cbfccb6cee4b001f66448
Saif Ali Ahmed <saifaliahmed@Saifs-MacBook-Pro.local>
2022-10-28 11:45:08 +0100

    added functionality

--- script.js
+++ script.js
@@ -7,29 +7,51 @@ import {
 const dietaryRequirements = document.querySelector("#dietary-require");
 const caloriesPref = document.querySelector("#calories");
 
-const createCard = (recipe, summary) => {
-	const div = document.createElement("div");
-	const img = document.createElement("img");
-	const title = document.createElement("h4");
-	const p = document.createElement("p");
 
-	div.setAttribute("id", "container");
-	img.src = recipe.image;
-	img.alt = `Image of ${recipe.title}`;
-	title.innerText = recipe.title;
-	p.innerText = summary;
 
-	div.append(img, title, p);
-	console.log(div);
-	return div;
-};
+const createCard = async (recipe) => {
+    const title = document.createElement('div')   
+    title.className = 'title'
+    document.getElementById('meal-results').appendChild(title)
+
+    const meal = document.createElement('div') 
+    title.appendChild(meal)
+    
+    const mealImage = document.createElement('div')
+    meal.appendChild(mealImage)
+
+    const image = document.createElement('img')
+    image.src = recipe.image
+    mealImage.appendChild(image)
+
+    const mealName = document.createElement('div')
+    meal.appendChild(mealName)
+
+    const mealTitle = document.createElement('h4')
+    mealTitle.innerText = recipe.title
+    mealName.appendChild(mealTitle)
+
+    const p = document.createElement('p')
+    p.innerText = await grabSummary(recipe.id)
+    mealName.appendChild(p)
+    
+
+    const a = document.createElement('a')
+    mealName.appendChild(a)
+}
+
+
+
 
 // The event listeners below are just for testing the output from the api for now.
 dietaryRequirements.addEventListener("change", async () => {
 	const dietary = await handleDietaryFetch();
-	const summary = await grabSummary(dietary.results[0].id);
-	console.log(dietary.results[0].id);
-	console.log(summary);
+    for (let i = 0; i < dietary.length; i++) {
+       createCard(dietary[i])
+
+     
+    }
+//	console.log(dietary)
 });
 
 caloriesPref.addEventListener("change", async () => {
