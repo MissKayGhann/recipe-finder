52f05935f0dbbc08f5f26dd3818b0fb961cee670
Abdullah Sahraoui <abdullahsahraoui@Abdullahs-MacBook-Air.local>
2022-10-28 14:27:20 +0100

    Fixed summary text.

--- script.js
+++ script.js
@@ -7,48 +7,43 @@ import {
 const dietaryRequirements = document.querySelector("#dietary-require");
 const caloriesPref = document.querySelector("#calories");
 
-
-
 const createCard = async (recipe) => {
-    const title = document.createElement('div')   
-    title.className = 'title'
-    document.getElementById('meal-results').appendChild(title)
+	const title = document.createElement("div");
+	title.className = "title";
+	document.getElementById("meal-results").appendChild(title);
 
-    const meal = document.createElement('div') 
-    title.appendChild(meal)
-    
-    const mealImage = document.createElement('div')
-    meal.appendChild(mealImage)
+	const meal = document.createElement("div");
+	title.appendChild(meal);
 
-    const image = document.createElement('img')
-    image.src = recipe.image
-    mealImage.appendChild(image)
+	const mealImage = document.createElement("div");
+	meal.appendChild(mealImage);
 
-    const mealName = document.createElement('div')
-    meal.appendChild(mealName)
+	const image = document.createElement("img");
+	image.src = recipe.image;
+	mealImage.appendChild(image);
 
-    const mealTitle = document.createElement('h4')
-    mealTitle.innerText = recipe.title
-    mealName.appendChild(mealTitle)
+	const mealName = document.createElement("div");
+	meal.appendChild(mealName);
 
-    const p = document.createElement('p')
-    p.innerText = await grabSummary(recipe.id)
-    mealName.appendChild(p)  
+	const mealTitle = document.createElement("h4");
+	mealTitle.innerText = recipe.title;
+	mealName.appendChild(mealTitle);
 
-    const a = document.createElement('a')
-    mealName.appendChild(a)
-}
+	const p = document.createElement("p");
+	p.innerHTML = await grabSummary(recipe.id);
+	mealName.appendChild(p);
 
-
-
+	const a = document.createElement("a");
+	mealName.appendChild(a);
+};
 
 // The event listeners below are just for testing the output from the api for now.
 dietaryRequirements.addEventListener("change", async () => {
 	const dietary = await handleDietaryFetch();
-    for (let i = 0; i < dietary.length; i++) {
-       createCard(dietary[i])
-    }
-//	console.log(dietary)
+	for (let i = 0; i < dietary.length; i++) {
+		createCard(dietary[i]);
+	}
+	//	console.log(dietary)
 });
 
 caloriesPref.addEventListener("change", async () => {
